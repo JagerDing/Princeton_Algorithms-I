9727309f17a092df5f695c283381e03451cd04f1
JAGER <jdingphy@gmail.com>
2018-01-04 10:49:01 -0700

    Update Permutation.java

--- 2. Deques and Randomized queues/Permutation.java
+++ 2. Deques and Randomized queues/Permutation.java
@@ -3,19 +3,15 @@ import java.util.Iterator;
 
 public class Permutation {
    public static void main(String[] args) {
-   	RandomizedQueue<String> a = new RandomizedQueue<String>();
-   	  System.out.println("Please input an integer:");
-   	  int k = StdIn.readInt();
-   	  System.out.println("Please input a sequence of strings:");
-  		while (StdIn.hasNextLine()) {
-  			String line = StdIn.readString();
-  			if (line.equals("---")) {break;}
-  			a.enqueue(line); 			
+   	RandomizedQueue<String> a = new RandomizedQueue<String>();   	 
+  		while (！StdIn.isEmpty()) {
+  			a.enqueue( StdIn.readString() );		
   		}
-  		Iterator<String> b = a.iterator();
-
+      
+  		int k = Integer.parseInt(args[0]);
+      Iterator<String> b = a.iterator();
   		for (int i = 0; i < k; i++) {
   			System.out.println(b.next());
   		}
-
-   }  
+   }
+}
