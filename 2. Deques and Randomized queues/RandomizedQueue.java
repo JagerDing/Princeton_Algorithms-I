f8afbcc7f3400c003fa93c5ea6046661bea28df7
JAGER <jdingphy@gmail.com>
2018-01-04 10:54:29 -0700

    Update RandomizedQueue.java

--- 2. Deques and Randomized queues/RandomizedQueue.java
+++ 2. Deques and Randomized queues/RandomizedQueue.java
@@ -3,107 +3,106 @@ import java.util.NoSuchElementException;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdIn;
 
-public class RandomizedQueue<Item> implements Iterable<Item>{
-	  private int N;   // number of items in the arrary
-	  private Item[] queue;
-	  private void resize(int capacity) {
-	  	Item[] copy = (Item[]) new Object[capacity];
-	  	for (int i = 0; i < N; i++) {
-	  		copy[i] = queue[i];
-	  	}
-	  	queue = copy;
-	  }
-	  
-	  public RandomizedQueue() {     //construct an empty randomized queue
-	  	N = 0;
-	    queue = (Item[]) new Object[2];
-	  }
-
-    public boolean isEmpty() {      //is the queue empty?
-    	return N == 0;  	
+public class RandomizedQueue<Item> implements Iterable<Item> {
+	private Item[] it = (Item[]) new Object[1];
+	private int count = 0;     //number of items including empty sites by dequeue
+	private int emptyNum = 0;  //number of empty sites by dequeue
+	
+  public RandomizedQueue() {
+  }
+  
+  public boolean isEmpty() { return count == 0;}
+  
+  public int size() {     //return the number of itmes on the dqueue
+  	return count-emptyNum;
+  }
+  
+  private void resize(int n) {
+  	Item[] temp = (Item[]) new Object[n];
+  	for (int i = 0; i < count; i++) {
+  		temp[i] = it[i];
   	}
-  	
-  	public int size() {         //return the number of items on the queue
-  		return N; 	
-  	}
-  	
-  	public void enqueue(Item item) {    // add item
-  		if (item == null) {
-  			throw new java.lang.IllegalArgumentException("can not add a null item.");
-  		}
-  		if (N == queue.length) {
-  			resize(2*N);
-  		}
-  	  queue[N++] = item;
-  	}
-  	
-  	public Item dequeue() {      //remove and return a  random item
-  		if (isEmpty()) {
-  			throw new java.util.NoSuchElementException();
-  		}
-  		int n = StdRandom.uniform(N);
-  		Item item = queue[n];
-      queue[n] = queue[--N];   // move the last item to the selected position
-      queue[N] = null;         // avoid loitering
-  		if (N > 0 && N == queue.length/4) {   //resize
-  			resize(queue.length/2);
-  		}
-  		return item;
-  	}     
-  	
-  	public Item sample() {       //return (but not remove) a random item
-  		if (isEmpty()) {
-  			throw new java.util.NoSuchElementException();
-  		}
-  		int n = StdRandom.uniform(N);
-  		Item item = queue[n];
-  		return item;
-  	}   
-  	
-  	public Iterator<Item> iterator() {   // return an independent iterator over items in random order
-  		return new ArrayIterator();
-  	} 
-  	
-  	private class ArrayIterator implements Iterator<Item> {
-  		private int num = N;
-  		public boolean hasNext() {
-  			return num > 0;
-  		}
-  		public void remove() {
-  			throw new java.lang.UnsupportedOperationException();
-  		}
-  		public Item next() {
-  			if (!hasNext()) {
-  				throw new java.util.NoSuchElementException();
-  			}
-  			int n = StdRandom.uniform(num);
-  			Item item = queue[n];
-  			Item temp = queue[n];
-  			queue[n] = queue[--num];
-  			queue[num] = temp; 
-  			return item;
+  	it = temp;
+  }
+  
+  public void enqueue(Item item) {
+  	if (item == null) {throw new IllegalArgumentException();}
+    if (count == it.length) { resize(2*count); }
+    it[count++] = item;
+  }
+   
+  private void shrink(int n) {  // shrink the array and delete the null sites
+  	Item[] temp = (Item[]) new Object[n];
+  	for (int i = 0, j = 0; i < count; i++) {
+  		if (it[i] != null) {
+  			temp[j++] = it[i];
   		}
   	}
+  	it = temp;
+  	count = count - emptyNum;
+  	emptyNum = 0;
+  }
+  
+  public Item dequeue() {  //remove and return a random item
+  	if (isEmpty()) {throw new NoSuchElementException();}
+  	int n = StdRandom.uniform(count);
+  	while (it[n] == null) { n = StdRandom.uniform(count); }
+  	Item temp = it[n];
+  	it[n] = null;
+  	emptyNum++;
+  	if ( count > 1 && (count-emptyNum) < 0.25*it.length ) {
+  		shrink(it.length/2);
+  	}
+  	return temp;
+  }
+  
+  public Item sample() {  //return a random item (but do not remove it)
+  	if (isEmpty()) {throw new NoSuchElementException();}
+        int n = StdRandom.uniform(count);
+        while (it[n] == null) { n = StdRandom.uniform(count); }
+        return it[n];
+  }
+  
+  public Iterator<Item> iterator() { // return an independent iterator over items in random order
+  	return new RQIterator();
+  }
+  
+  private class RQIterator implements Iterator<Item> {
+  	private int rank = count - emptyNum;
+        private Item[] temp = (Item[]) new Object[count-emptyNum];
+        private RQIterator() {
+          for (int i = 0, j = 0; i < count; i++) {
+  		  if (it[i] != null) {
+  			  temp[j++] = it[i];
+  		  } 
+  	}
   	
-  	public static void main(String[] args) {      // unit testing
-  		RandomizedQueue<String> a = new RandomizedQueue<String>();
-  		while (StdIn.hasNextLine()) {
-  			String line = StdIn.readString();
-  			if (line.equals("---")) {break;}
-  			a.enqueue(line); 			
-  		}
-  		Iterator<String> b = a.iterator();
-  		while (b.hasNext()) {
-  			System.out.println(b.next());
-  		}
-  		System.out.println("***********");
-  		for (int i = 0; i < 5; i++) {
-  			System.out.println(a.dequeue());
-  		}
-  		System.out.println("@@@@@@@@@");
-  		Iterator<String> c = a.iterator();
-  		while (c.hasNext()) {
-  			System.out.println(c.next());
-  		}
-  	}  
+  	public boolean hasNext() { return rank > 0; }
+  	public void remove() { throw new UnsupportedOperationException();}
+  	public Item next() {
+          if (rank == 0) {throw new NoSuchElementException();}
+          int n = StdRandom.uniform(rank--);
+          Item a = temp[n];
+          temp[n] = temp[rank];
+          temp[rank] = a;
+          return a;
+  	}
+  }
+  
+  public static void main(String[] args) {
+  	     RandomizedQueue<Integer> rq1 = new RandomizedQueue<Integer>();
+  	    // RandomizedQueue<Integer> rq2 = new RandomizedQueue<Integer>();
+         System.out.println(rq1.isEmpty());
+         System.out.println(rq1.size());
+        
+         //rq2.isEmpty();
+         rq1.enqueue(1);
+         System.out.println(rq1.isEmpty());
+         //rq1.enqueue(20);
+        
+         System.out.println(rq1.dequeue());
+         System.out.println(rq1.count+" "+rq1.emptyNum);
+          rq1.enqueue(0);
+         System.out.println(rq1.size());
+  }
 }
